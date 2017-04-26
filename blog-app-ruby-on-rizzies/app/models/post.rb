f80b0070a3d8e0782b1e8d32cec3d6ddc790736e
Lamar Smith <Lamar5503@me.com>
2017-04-26 00:00:30 -0700

    added upload photo feature to post

--- blog-app-ruby-on-rizzies/app/models/post.rb
+++ blog-app-ruby-on-rizzies/app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5}
   validates :body, presence: true
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
